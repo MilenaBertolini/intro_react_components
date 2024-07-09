9de9668fbe3187d53122a11d8022acfa60fc3103
MilenaBertolini <milenaarantesb@gmail.com>
2024-07-09 16:20:08 -0300

    subindo lista 05

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import Informacao from './componentes/Informacao';
+import CaixaInfo from './componentes/CaixaInfo';
+
+function App() {
+
+  return (
+
+    <>
+      <Informacao/>
+      <CaixaInfo texto='Plano de desenvolvimento institucional'/>
+    </>
+
+  );
+
+}
+
+export default App;
